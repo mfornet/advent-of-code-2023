46297fddd14e3fffa1c48b464f3836cfcd069571
Marcelo Fornet <mfornet94@gmail.com>
2023-12-25 12:32:19 +0100

    day 25 -- test

--- src/bin/25.rs
+++ src/bin/25.rs
@@ -134,7 +134,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(54));
     }
 
     #[test]
